b083b1ffcbb013ace81f5c44edc0b05b4058d928
gaconkzk <gaconkzk@gmail.com>
2019-11-12 12:48:43 +0700

    temp stop

--- src/bin/req.rs
+++ src/bin/req.rs
@@ -2,9 +2,13 @@ use reqwest::Error;
 
 use fshare;
 
+use dotenv::dotenv;
+
 fn main() -> Result<(), Error> {
-  let matches = fshare::cmd::make();
-  let api = &mut fshare::make(&matches);
+  dotenv().ok();
+
+  let config = fshare::cmd::make();
+  let api = &mut fshare::make(&config);
 
   api.login().unwrap_or_else(|error| {
     panic!("Error: {}", error);
--- src/lib/fshare.rs
+++ src/lib/fshare.rs
@@ -1,68 +1,101 @@
 use serde:: { Serialize };
 
+use serde_json:: { Value };
+
 use clap:: {ArgMatches };
 
 use reqwest:: blocking:: { Client };
-use reqwest:: { Error };
-
-use std::collections::HashMap;
+use reqwest:: { StatusCode, Error };
 
 pub struct FsApi {
   email: String,
   password: String,
-  token: &'static str,
-  session: &'static str, // session
+  vip: bool,
+  token: String,
+  session: String, // session
 }
 
 #[derive(Serialize, Debug)]
-pub struct FsRequest {
-  user_email: String,
-  password: String,
-  app_key: String,
+pub struct FsVipRequest<'a> {
+  user_email: &'a str,
+  password: &'a str,
+  app_key: &'a str,
 }
 
-fn _create(email: String, password: String) -> FsApi {
+fn _create(email: String, password: String, vip: bool) -> FsApi {
   FsApi {
     email: email,
     password: password,
-    token: "",
-    session: "",
+    token: String::from(""),
+    session: String::from(""),
+    vip: vip,
   }
 }
 
 pub fn make(matches: &ArgMatches) -> FsApi {
   let email = matches.value_of("email").unwrap();
   let password = matches.value_of("password").unwrap();
-
-  _create(String::from(email), String::from(password))
+  let vip = matches.value_of("isvip");
+  _create(String::from(email), String::from(password), vip.unwrap().parse().unwrap())
 }
+
+pub trait FsLogin {
+  fn login(&mut self);
+}
+
+
 
 impl FsApi {
   pub fn login(&mut self) -> Result<(), Error> {
+    if self.vip {
+      self._login_vip()?
+    } else {
+      self._login_normal()?
+    }
+
+    Ok(())
+  }
+
+  fn _login_vip(&mut self) -> Result<(), Error> {
     let request_url = format!("https://api.fshare.vn:443/api/user/login");
 
-    let mut map = HashMap::new();
-    map.insert("user_email", self.email.as_ref());
-    map.insert("password", self.password.as_ref());
-    map.insert("app_key", "L2S7R6ZMagggC5wWkQhX2+aDi467PPuftWUMRFSn");
+    let request = FsVipRequest {
+      user_email: self.email.as_ref(),
+      password: self.password.as_ref(),
+      app_key: "L2S7R6ZMagggC5wWkQhX2+aDi467PPuftWUMRFSn",
+    };
 
     let client = Client::new();
 
     let res = client.post(&request_url)
-      .json(&map)
-      // .header("User-Agent", "okhttp/3.6.0")
-      .send()?.text()?;
+      .json(&request)
+      .send()?;
 
-    println!("{:?}", res);
+    let status = res.status();
+    let data = &res.text()?;
+    let v: Value = serde_json::from_str(data.as_ref()).unwrap();
+
+    match status {
+      StatusCode::OK => {
+        self.session = v["session_id"].as_str().unwrap().to_string();
+        self.token = v["token"].as_str().unwrap().to_string();
+
+        println!("{:?}", v);
+      },
+      s => {
+        let code: u64 = v["code"].as_u64().unwrap();
+        let msg: &str = v["msg"].as_str().unwrap();
+        println!("Received response status: {:?}", s);
+        println!("Code: {}", code);
+        println!("Message: {}", msg);
+      },
+    }
 
     Ok(())
-    // let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
-    //                           owner = "gaconkzk",
-    //                           repo = "buom");
-    // println!("{}", request_url);
-    // let mut response = reqwest::get(&request_url)?;
+  }
 
-    // let users: Vec<User> = response.json()?;
-    // println!("{:?}", users);
+  fn _login_normal(&mut self) -> Result<(), Error> {
+    println!("Not implement yet");
+    Ok(())
   }
 }
